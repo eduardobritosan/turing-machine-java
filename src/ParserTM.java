9ed6ee32607ed89326faec9a9e59fe998e98e99b
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2015-10-09 19:35:31 +0100

    First commit

---
+++ src/ParserTM.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author eebritos
+ *
+ */
+public class ParserTM extends Parser {
+
+	/**
+	 * @param filename
+	 */
+	public ParserTM(String filename) {
+		super(filename);
+	}
+	
+	private void transformParsedData(){
+		
+	}
+	
+}
